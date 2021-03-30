3190187290d988f139c5ce9b3125727acc08b18d
Seigo Jinbo <seigojinbo@gmail.com>
2021-03-30 16:49:51 +0900

    remove gem

--- app/serializers/game_serializer.rb
+++
@@ -1,5 +0,0 @@
-class GameSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :bet, :start, :rotation, :quantity, :value, :turn, :max, :user_ids
-  has_many :users
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
   get 'users/edit'
   devise_for :users
   resources :games
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users
-  #     resources :games
-  #   end
-  # end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
